3cda90243a867d1b74feaa51c9e2c9f70f49715f
lcolmenarejo <lcolmenarejo@bitbucket.org>
2018-11-20 19:49:41 +0100

    HOLI

---
+++ p4/workflowrepository/data/models.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import django
+from django.db import models
+from django.template.defaultfilters import slugify
+
+# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=128,unique=True, blank=False)
+	slug = models.SlugField(unique=True)
+	created = models.DateField(auto_now=True)
+	tooltip = models.CharField(max_length=128)
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
+
+	class Meta:
+		verbose_name_plural = 'categories'
+
+	def __str__(self):
+		return self.name
+
+class Workflow(models.Model):
+	name = models.CharField(max_length=128, unique=True, blank=False)
+	slug = models.SlugField(unique=True)
+	description = models.CharField(max_length=512, default="")
+	views = models.IntegerField(default=0)
+	downloads = models.IntegerField(default=0)
+	versionInit = models.CharField(max_length=512)
+	category = models.ManyToManyField(Category, blank=False)
+	client_ip = models.GenericIPAddressField()
+	keywords = models.CharField(max_length=256, default = "")
+	json = models.TextField(max_length=512)
+	created = models.DateField(auto_now=True)
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Workflow, self).save(*args, **kwargs)
+
+	def __str__(self): # For Python 2, use __unicode__ too
+		return self.name
+
+	def __unicode__(self):
+	        return self.name
---
+++ p4/workflowrepository/find/urls.py
@@ -0,0 +1,27 @@
+"""workflowrepository URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+
+from find import views
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='workflow_list'),
+    url(r'^base/', views.base, name='base'),
+    url(r'^workflow_list/(?P<category_slug>[-\w]+)/$', views.workflow_list, name='workflow_list_by_category'),
+    url(r'^workflow_detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.workflow_detail, name='workflow_detail'),
+    url(r'^workflow_search/$', views.workflow_search, name='workflow_search'),
+]
